dc571f5cd14e1d2f0138f922c431f0a5508db44a
PEmilio <palomillo.flores@gmail.com>
2025-12-03 02:15:12 -0600

    Refactor login method and remove password hashing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,37 +15,24 @@ export class LoginComponent {
   password: string = '';
 
   users = [
-    { username: 'law',  passwordHash: '7d3ea98b27659b2f45f9a1ab6f6b5bbec1a2bb8f1a77a22fce6473f1d7226c97' }, 
-    { username: 'zoro', passwordHash: 'f2f98ee8f06c7e75bfe9a1e4a2d48ce2af2bff8d5c9c5b1df8b93bb5c0a8f6c5' }
+    { username: 'law', password: 'corazon' },
+    { username: 'zoro', password: 'kuina' }
   ];
 
   constructor(private router: Router) {}
 
-  async login() {
-
-    const inputHash = await this.hashPassword(this.password);
+  login() {
+    console.log('Intento login:', this.username, this.password);
 
     const validUser = this.users.find(
-      u =>
-        u.username === this.username &&
-        u.passwordHash === inputHash
+      u => u.username === this.username && u.password === this.password
     );
 
     if (validUser) {
-      console.log('Login correcto (password protegida)');
+      console.log('Login correcto');
       this.router.navigateByUrl('/dashboard');
     } else {
       alert('Usuario o contraseña incorrectos');
     }
-  }
-
-  async hashPassword(password: string): Promise<string> {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
   }
 }
